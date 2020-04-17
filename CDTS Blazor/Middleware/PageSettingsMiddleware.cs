57d62fe2ca49c62238a19b4e69e9e4a9d3f3446c
Tibo <khoi.phung@tc.gc.ca>
2020-04-17 03:03:58 -0400

    AB#1 migrate code to GitHub

---
+++ CDTS Blazor/Middleware/PageSettingsMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using GoC.WebTemplate.Components.Core.Services;
+using GoC.WebTemplate.Components.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CDTS_Blazor.Classes
+{
+    public class PageSettingsMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public PageSettingsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, ModelAccessor modelAccessor)
+        {
+            //add page settings like: Modified Date, Breadcrumbs, Culture, Title etc.
+            modelAccessor.Model.DateModified = DateTime.Now.Date;
+            modelAccessor.Model.HeaderTitle = "Transport Canada";
+            List<Breadcrumb> breadcrumbs = new List<Breadcrumb>();
+            breadcrumbs.Add(new Breadcrumb { Href = "http://www.canada.ca/en/index.html", Title = "Home" });
+            breadcrumbs.Add(new Breadcrumb { Href = "https://www.tc.gc.ca/en/services/marine.html", Title = "Marine Transportation" });
+            breadcrumbs.Add(new Breadcrumb { Href = "https://www.tc.gc.ca/eng/marinesafety/oep-menu.htm", Title = "Operation & Environment" });
+            modelAccessor.Model.Breadcrumbs = breadcrumbs;
+            modelAccessor.Model.LeftMenuItems = new List<MenuSection>();
+
+            await _next.Invoke(context);
+        }
+    }
+    public static class PageSettingsMiddlewareExtension
+    {
+        public static IApplicationBuilder UsePageSettingsMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<PageSettingsMiddleware>();
+        }
+    }
+}
