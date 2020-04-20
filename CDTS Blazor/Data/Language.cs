458c6cc3586fb34ac4cde4bc5b1e2080780816b4
AlexandreSanscartier <alexandre.sanscartier@codefor.ca>
2020-04-20 15:36:23 -0400

    Added the cdnform and the tombstone information text inputs and labels

---
+++ CDTS Blazor/Data/Language.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CDTS_Blazor.Data
+{
+    /// <summary>
+    /// Enumeration for languages
+    /// </summary>
+    public enum Language
+    {
+        /// <summary>
+        /// Defines the english language
+        /// </summary>
+        ENGLISH,
+
+        /// <summary>
+        /// Defines the french language
+        /// </summary>
+        FRENCH
+    }
+}
---
+++ CDTS Blazor/TCComponents/TCInputText.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Linq.Expressions;
+
+namespace CDTS_Blazor.TCComponents
+{
+    public class TCInputTextBase : InputBase<string>
+    {
+        [Parameter] public string Id { get; set; }
+        [Parameter] public string Label { get; set; }
+        [Parameter] public Expression<Func<string>> ValidationFor { get; set; }
+
+        protected override bool TryParseValueFromString(string value, out string result, out string validationErrorMessage)
+        {
+            result = value;
+            validationErrorMessage = null;
+            return true;
+        }
+    }
+}
