5a25c46e817ff2cfc4176a58e45319b03c041e50
Tibo <khoi.phung@tc.gc.ca>
2020-04-21 09:53:36 -0400

    AB#8 added all fields and validations using WET

---
+++ CDTS Blazor/Data/Entity/Address.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CDTS_Blazor.Data.Entity
+{
+    public class Address
+    {
+        public string ApartmentUnitNumber { get; set; }
+
+        [Required]
+        public string StreetNumber { get; set; }
+
+        [Required]
+        public string Street { get; set; }
+
+        public string POBox { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Province { get; set; }
+
+        [Required]
+        public string PostalCode { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        public string Telephone { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
