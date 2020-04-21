0470b49d7ff3f3dfabd49879d13c7a9b67f3e393
AlexandreSanscartier <alexandre.sanscartier@codefor.ca>
2020-04-21 15:58:32 -0400

    Added the HTMLInputTypes and the way to implement it for the TCInputText

---
+++ CDTS Blazor/TCComponents/HtmlInputTypes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CDTS_Blazor.TCComponents
+{
+    /// <summary>
+    /// Enumeration for languages
+    /// </summary>
+    public enum HtmlInputType
+    {
+
+    }
+
+    /// <summary>
+    /// Used to define the input type for the TCInputText
+    /// </summary>
+    public enum TextInputType
+    {
+        DATE,
+
+        DATETIME_LOCAL,
+
+        EMAIL,
+
+        MONTH,
+
+        NUMBER,
+
+        PASSWORD,
+
+        SEARCH,
+
+        TEL,
+
+        TEXT,
+
+        TIME,
+
+        URL,
+
+        WEEK
+    }
+}
--- CDTS Blazor/TCComponents/TCInputText.razor.cs
+++ CDTS Blazor/TCComponents/TCInputText.razor.cs
@@ -9,6 +9,7 @@ namespace CDTS_Blazor.TCComponents
     {
         [Parameter] public string Id { get; set; }
         [Parameter] public string Label { get; set; }
+        [Parameter] public TextInputType TextInputType { get; set; } = TextInputType.TEXT;
         [Parameter] public Expression<Func<string>> ValidationFor { get; set; }
 
         protected override bool TryParseValueFromString(string value, out string result, out string validationErrorMessage)
@@ -16,6 +17,38 @@ namespace CDTS_Blazor.TCComponents
             result = value;
             validationErrorMessage = null;
             return true;
+        }
+
+        public string TextInputTypeString()
+        {
+            switch(this.TextInputType)
+            {
+                case TextInputType.DATE:
+                    return "date";
+                case TextInputType.DATETIME_LOCAL:
+                    return "datetime-local";
+                case TextInputType.EMAIL:
+                    return "email";
+                case TextInputType.MONTH:
+                    return "month";
+                case TextInputType.NUMBER:
+                    return "number";
+                case TextInputType.PASSWORD:
+                    return "password";
+                case TextInputType.SEARCH:
+                    return "search";
+                case TextInputType.TEL:
+                    return "tel";
+                case TextInputType.TIME:
+                    return "time";
+                case TextInputType.URL:
+                    return "url";
+                case TextInputType.WEEK:
+                    return "week";
+                case TextInputType.TEXT:
+                default:
+                    return "text";
+            }
         }
     }
 }
