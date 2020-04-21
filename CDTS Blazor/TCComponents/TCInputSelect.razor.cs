d2884079e5ce5637163eb82a94f2106b3856cf3a
AlexandreSanscartier <alexandre.sanscartier@codefor.ca>
2020-04-21 13:12:55 -0400

    Added the aurora design system to the CDN Application form. Added a custom input for select control

---
+++ CDTS Blazor/TCComponents/TCInputSelect.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Globalization;
+using System.Linq.Expressions;
+
+namespace CDTS_Blazor.TCComponents
+{
+    public class TCInputSelectBase<T> : InputBase<T>
+    {
+        [Parameter] public string Id { get; set; }
+        [Parameter] public string Label { get; set; }
+        [Parameter] public Expression<Func<T>> ValidationFor { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+        [Parameter] public bool ShowDefaultOption { get; set; } = true;
+
+        protected override bool TryParseValueFromString(string value, out T result, out string validationErrorMessage)
+        {
+            if (typeof(T) == typeof(string))
+            {
+                result = (T)(object)value;
+                validationErrorMessage = null;
+                return true;
+            }
+            else if (typeof(T).IsEnum)
+            {
+                var success = BindConverter.TryConvertTo<T>(value, CultureInfo.CurrentCulture, out var parsedValue);
+                if (success)
+                {
+                    result = parsedValue;
+                    validationErrorMessage = null;
+                    return true;
+                }
+                else
+                {
+                    result = default;
+                    validationErrorMessage = $"The {FieldIdentifier.FieldName} field is not valid.";
+                    return false;
+                }
+            }
+
+            throw new InvalidOperationException($"{GetType()} does not support the type '{typeof(T)}'.");
+        }
+    }
+}
