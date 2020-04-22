ba493cf35a14fbf12e1a4017ec3d59b767d24c22
Tibo <khoi.phung@tc.gc.ca>
2020-04-22 07:58:39 -0400

    AB#18 Added Aurora Styled Progress Bars

---
+++ CDTS Blazor/Data/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CDTS_Blazor.Data
+{
+    public class Constants
+    {
+        public const string ProgressBarText1 = "Applicant Details";
+        public const string ProgressBarText2 = "Mailing Address";
+        public const string ProgressBarText3 = "Identity Documents";
+        public const string ProgressBarText4 = "Remarks and Signature";
+    }
+}
