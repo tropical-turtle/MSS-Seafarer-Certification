f293681ccbba5bc3c658ed95e42b7185efb149bd
Chris ikongo <chris.ikongo@034gc.onmicrosoft.com>
2020-06-08 14:42:00 -0400

    Added Azure key vault tests

---
+++ CDNApplication.Test/AzureKeyVaultTests.cs
@@ -0,0 +1,40 @@
+﻿using CDNApplication.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CDNApplication.Test
+{
+    public class AzureKeyVaultTests
+    {
+
+        private AzureKeyVaultService azureKeyVaultService;
+
+        public AzureKeyVaultTests()
+        {
+            azureKeyVaultService = new AzureKeyVaultService("https://kv-seafarer-dev.vault.azure.net/");
+        }
+
+        [Theory]
+        [InlineData("BlobStorage")]
+        public void GetSerectByName_ReturnsSerect(string serectName)
+        {
+            var serect = azureKeyVaultService.GetSecretByName(serectName);
+
+            Assert.NotNull(serect);
+        }
+
+        [Fact]
+        public void GetSerects_ReturnsAListOfSerects()
+        {
+            var serect = azureKeyVaultService.GetListOfSecrets();
+
+            Assert.NotNull(serect);
+
+            Assert.NotEmpty(serect);
+
+        }
+
+    }
+}
